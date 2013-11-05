166ef0a5737908c4d86f4d19de42ff0937a1e515
Oskar Separovic <oskar.separovic@corp.hookt.com>
2013-11-05 10:56:08 -0800

    Added .gitignore and xib and ripped out many remaining references and unused methods and properties. Still needs much cleanup and tweaking.

--- MessageComposerView.h
+++ MessageComposerView.h
@@ -1,39 +1,23 @@
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 @protocol MessageComposerViewDelegate <NSObject>
-
 - (void)messageComposerSendMessageClickedWithMessage:(NSString*)message;
 - (void)messageComposerFrameDidChange:(CGRect)frame withAnimationDuration:(float)duration;
-
 @optional
 - (void)messageComposerUserTyping;
-
 @end
 
-@interface MessageComposerView : UIView<EmoticonReactionKeyboardViewDelegate, ImagePickerDelegate, UITextViewDelegate>
-
-- (void)showSendButton;
-
+@interface MessageComposerView : UIView<UITextViewDelegate>
 @property(nonatomic, strong) id<MessageComposerViewDelegate> delegate;
-
-@property(nonatomic, strong) IBOutlet PlaceHolderTextView* messageTextView;
-@property(nonatomic, strong) IBOutlet UIProfileImageView* profileButton;
-@property(nonatomic, strong) IBOutlet UIButton* sendButton;
-@property(nonatomic, strong) IBOutlet UIButton* cameraButton;
-@property(nonatomic, strong) IBOutlet UIButton* emojiButton;
-@property(nonatomic, strong) IBOutlet UIView* textFieldContainerView;
-@property(nonatomic, strong) IBOutlet UIImageView* textFieldImageView;
-@property(nonatomic, strong) IBOutlet UIImageView* chatBubbleTail;
-@property(nonatomic, strong) IBOutlet NSString *messageTextPlaceholder;
-@property(nonatomic, strong) EmoticonReactionKeyboardView* emoticonReactionKeyboard;
-@property(nonatomic) BOOL showingCustomKeyboard;
+@property(nonatomic, strong) IBOutlet UITextView *messageTextView;
+@property(nonatomic, strong) IBOutlet UIButton *sendButton;
+@property(nonatomic, strong) IBOutlet UIView *textFieldContainerView;
+@property(nonatomic, strong) IBOutlet UIImageView *textFieldImageView;
+@property(nonatomic, strong) NSString *messageTextPlaceholder;
 @property(nonatomic) BOOL showingSendButton;
-@property(nonatomic, strong) ImagePickerController* imagePicker;
-
-- (void)toggleCustomKeyboardButtonsHidden:(BOOL)hidden;;
-- (void)showCustomKeyboard;
-- (void)hideCustomKeyboard;
 - (IBAction)sendClicked:(id)sender;
+- (void)showSendButton;
 // needed for loading drafts
 - (void)resizeTextViewForText:(NSString*)text;
 - (void)scrollTextViewToBottom;
--- MessageComposerView.m
+++ MessageComposerView.m
@@ -11,18 +11,15 @@
 
 @implementation MessageComposerView
 
+const int KEYBOARD_PORTRAIT_HEIGHT = 216;
+const int KEYBOARD_LANDSCAPE_HEIGHT = 162;
+
 const int kTextViewTopOffset = 6;
 const int kMessageBubbleInitialTopOffset = 10;
 const int kMessageBubbleInitialHeight = 70;
 
 const int kSendButtonWidth = 70;
 const int kSendButtonActiveRightOffset = 5;
-
-const int kProfileImageActiveRightOffset = 65;
-const int kProfileImageInactiveRightOffset = 6;
-
-const int kChatBubbleTailActiveRightOffset = 102;
-const int kChatBubbleTailInactiveRightOffset = 42;
 
 - (id)initWithFrame:(CGRect)frame
 {
@@ -36,23 +33,9 @@ const int kChatBubbleTailInactiveRightOffset = 42;
 - (void)awakeFromNib {
     [super awakeFromNib];
     [self addNotifications];
-    self.showingCustomKeyboard = FALSE;
-    self.messageTextPlaceholder = NSLocalizedString(@"LABEL.PLACEHOLDER.TYPE.MESSAGE", nil);
+    self.messageTextPlaceholder = @"Type message...";
     self.messageTextView.showsHorizontalScrollIndicator = NO;
-    
-    self.textFieldImageView.image = [UIImage textFieldImage];
-    
-    [self.sendButton setBackgroundImage:[UIImage sendButtonImage] forState:UIControlStateNormal];
-    [self.sendButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.sendButton.titleLabel setFont:[UIFont helveticaNeueRegular12]];
-    [self.sendButton setTitle:NSLocalizedString(@"BUTTON.LABEL.SEND", nil) forState:UIControlStateNormal];
-    
-    self.chatBubbleTail.image = [UIImage messageComposerTail];
-    
-    [self.cameraButton setBackgroundImage:[UIImage cameraButtonImage] forState:UIControlStateNormal];
-    [self.cameraButton setBackgroundImage:[UIImage cameraButtonImageSelected] forState:UIControlStateSelected];
-    [self.cameraButton setBackgroundImage:[UIImage cameraButtonImageSelected] forState:UIControlStateHighlighted];
-    
+
     //make the background image view clickable
     UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(textBackgroundClicked:)];
     singleTap.numberOfTapsRequired = 1;
@@ -63,11 +46,6 @@ const int kChatBubbleTailInactiveRightOffset = 42;
 
 - (void)dealloc {
     [self removeNotifications];
-}
-
-- (void)toggleCustomKeyboardButtonsHidden:(BOOL)hidden {
-    self.cameraButton.hidden = hidden;
-    self.emojiButton.hidden = hidden;
 }
 
 
@@ -99,13 +77,8 @@ const int kChatBubbleTailInactiveRightOffset = 42;
 }
 
 - (void)textViewDidBeginEditing:(UITextView*)textView {
-    if(self.showingCustomKeyboard) {
-        self.showingCustomKeyboard = FALSE;
-        [self hideCustomKeyboard];
-    }
-    
     float keyboardHeight;
-    if (UIInterfaceOrientationIsPortrait([BaseAppDelegate currentOrientation])) {
+    if (UIInterfaceOrientationIsPortrait([self currentOrientation])) {
         keyboardHeight = KEYBOARD_PORTRAIT_HEIGHT;
     } else {
         keyboardHeight = KEYBOARD_LANDSCAPE_HEIGHT;
@@ -138,13 +111,13 @@ const int kChatBubbleTailInactiveRightOffset = 42;
 }
 
 - (void)handleRotation:(NSNotification*)notification {
-    if(self.showingCustomKeyboard || [self.messageTextView isFirstResponder]) {
+    if([self.messageTextView isFirstResponder]) {
         NSDictionary* userInfo = [notification userInfo];
         float animationTime = [userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue];
         
         CGRect frame = self.frame;
 
-        if (UIInterfaceOrientationIsPortrait([BaseAppDelegate currentOrientation])) {
+        if (UIInterfaceOrientationIsPortrait([self currentOrientation])) {
             frame.origin.y = (self.superview.frame.size.height - KEYBOARD_PORTRAIT_HEIGHT) - self.frame.size.height;
         } else {
             frame.origin.y = (self.superview.frame.size.height - KEYBOARD_LANDSCAPE_HEIGHT) - self.frame.size.height;
@@ -156,66 +129,9 @@ const int kChatBubbleTailInactiveRightOffset = 42;
         if(self.delegate) {
             [self.delegate messageComposerFrameDidChange:frame withAnimationDuration:animationTime];
         }
-        
-        [self.emoticonReactionKeyboard reconfigurePageView];
     }
 }
 
-#pragma mark - custom keyboard display
-- (void)showCustomKeyboard {
-    self.showingCustomKeyboard = TRUE;
-    [self.emoticonReactionKeyboard removeFromSuperview];
-    
-    [self.messageTextView resignFirstResponder];
-    
-    CGRect frame = self.emoticonReactionKeyboard.frame;
-    frame.origin.x = 0;
-    frame.origin.y = self.superview.frame.size.height;
-    self.emoticonReactionKeyboard.frame = frame;
-    [self.superview addSubview:self.emoticonReactionKeyboard];
-    
-    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-        CGRect frame = self.frame;
-        if ([BaseAppDelegate currentOrientation] == UIInterfaceOrientationPortrait) {
-            frame.origin.y = [BaseAppDelegate currentSize].height - KEYBOARD_PORTRAIT_HEIGHT - self.frame.size.height - 44;
-        } else {
-            frame.origin.y = [BaseAppDelegate currentSize].height - KEYBOARD_LANDSCAPE_HEIGHT - self.frame.size.height;
-        }
-        self.frame = frame;
-    } completion:^(BOOL finished) {}];
-    
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect frame = self.emoticonReactionKeyboard.frame;
-        frame.origin.y = self.superview.frame.size.height - self.emoticonReactionKeyboard.frame.size.height;
-        self.emoticonReactionKeyboard.frame = frame;
-    }];
-    
-    if(self.delegate) {
-        [self.delegate messageComposerFrameDidChange:self.frame withAnimationDuration:0.3];
-    }
-}
-
-- (void)hideCustomKeyboard {
-    self.showingCustomKeyboard = FALSE;
-    
-    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-        CGRect frame = self.frame;
-        frame.origin.y = self.superview.frame.size.height - self.frame.size.height;
-        self.frame = frame;
-    } completion:^(BOOL finished) {}];
-    
-    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-        CGRect frame = self.emoticonReactionKeyboard.frame;
-        frame.origin.y = self.superview.frame.size.height;
-        self.emoticonReactionKeyboard.frame = frame;
-    } completion:^(BOOL finished) {
-        [self.emoticonReactionKeyboard removeFromSuperview];
-    }];
-    
-    if(self.delegate) {
-        [self.delegate messageComposerFrameDidChange:self.frame withAnimationDuration:0.3];
-    }
-}
 
 #pragma mark - TextView Frame Manipulation
 - (void)resizeTextViewForText:(NSString*)text {
@@ -296,6 +212,10 @@ const int kChatBubbleTailInactiveRightOffset = 42;
     }
 }
 
+- (void)textBackgroundClicked:(id)sender {
+    [self.messageTextView becomeFirstResponder];
+}
+
 - (void)scrollTextViewToBottom {
     [self.messageTextView scrollRangeToVisible:NSMakeRange([self.messageTextView.text length], 0)];
 }
@@ -314,26 +234,6 @@ const int kChatBubbleTailInactiveRightOffset = 42;
                          self.sendButton.frame = frame;
                      }
                      completion:nil];
-    
-    frame = self.profileButton.frame;
-    frame.origin.x = self.frame.size.width - self.profileButton.frame.size.width - kProfileImageActiveRightOffset;
-    [UIView animateWithDuration:0.6
-                          delay:0
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^{
-                         self.profileButton.frame = frame;
-                     }
-                     completion:nil];
-    
-    frame = self.chatBubbleTail.frame;
-    frame.origin.x = self.frame.size.width - self.chatBubbleTail.frame.size.width - kChatBubbleTailActiveRightOffset;
-    [UIView animateWithDuration:0.6
-                          delay:0
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^{
-                         self.chatBubbleTail.frame = frame;
-                     }
-                     completion:nil];
 }
 
 - (void)hideSendButton {
@@ -348,45 +248,15 @@ const int kChatBubbleTailInactiveRightOffset = 42;
                          self.sendButton.frame = frame;
                      }
                      completion:nil];
-    
-    frame = self.profileButton.frame;
-    frame.origin.x = self.frame.size.width - self.profileButton.frame.size.width - kProfileImageInactiveRightOffset;
-    [UIView animateWithDuration:0.6
-                          delay:0
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^{
-                         self.profileButton.frame = frame;
-                     }
-                     completion:nil];
-    
-    frame = self.chatBubbleTail.frame;
-    frame.origin.x = self.frame.size.width - self.chatBubbleTail.frame.size.width - kChatBubbleTailInactiveRightOffset;
-    [UIView animateWithDuration:0.6
-                          delay:0
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^{
-                         self.chatBubbleTail.frame = frame;
-                     }
-                     completion:nil];
 }
 
 - (void)setMessageTextPlaceholder:(NSString *)messageTextPlaceholder {
     _messageTextPlaceholder = messageTextPlaceholder;
-    self.messageTextView.placeholder = messageTextPlaceholder;
+//    self.messageTextView.placeholder = messageTextPlaceholder;
 }
 
 
 #pragma mark - IBAction
-- (IBAction)emoticonButtonClicked:(id)sender {
-    if(self.delegate) {
-        if(self.showingCustomKeyboard) {
-            [self hideCustomKeyboard];
-        } else {
-            [self showCustomKeyboard];
-        }
-    }
-}
-
 - (IBAction)sendClicked:(id)sender {
     if(self.delegate) {
         [self.delegate messageComposerSendMessageClickedWithMessage:self.messageTextView.text];
@@ -400,5 +270,11 @@ const int kChatBubbleTailInactiveRightOffset = 42;
         // UITextViewTextDidChangeNotification notification does not get fired.
         [self textViewTextDidChange:nil];        
     }
+}
+
+
+#pragma mark - Utils
+- (UIInterfaceOrientation)currentOrientation {
+    return [UIApplication sharedApplication].statusBarOrientation;
 }
 @end
