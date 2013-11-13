e8489c9d8c740c56cc07df603bf3c2e6a31851a6
Oskar Separovic <oskar.separovic@corp.hookt.com>
2013-11-12 21:21:20 -0800

    Added copyright notice and removed last few unused properties

--- MessageComposerView.h
+++ MessageComposerView.h
@@ -1,3 +1,25 @@
+// MessageComposerView.h
+//
+// Copyright (c) 2013 oseparovic. ( http://thegameengine.org )
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 #import <QuartzCore/QuartzCore.h>
@@ -13,14 +35,10 @@
 @property(nonatomic, strong) id<MessageComposerViewDelegate> delegate;
 @property(nonatomic, strong) IBOutlet UITextView *messageTextView;
 @property(nonatomic, strong) IBOutlet UIButton *sendButton;
-@property(nonatomic, strong) IBOutlet UIView *textFieldContainerView;
-@property(nonatomic, strong) NSString *messageTextPlaceholder;
-@property(nonatomic) BOOL showingSendButton;
 - (IBAction)sendClicked:(id)sender;
 - (void)resizeTextViewForText:(NSString*)text;
 - (void)resizeTextViewForText:(NSString*)text animated:(BOOL)animated;
 - (void)scrollTextViewToBottom;
-
 // to avoid exposing the UITextView and attempt to prevent bad practice, startEditing and finishEditing
 // are available to become and resign first responder. This means you shouldn't have an excuse to
 // do [messageComposerView.messageTextView resignFirstResponder] etc.
